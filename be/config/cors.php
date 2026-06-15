592b822f076767a60e19e15f7673296b24f25fb6
dqtung1703 <doquangtung19@gmail.com>
2026-06-15 17:08:51 +0700

    fix(be): allow frontend origin from environment

--- be/config/cors.php
+++ be/config/cors.php
@@ -22,9 +22,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
+    'allowed_origins' => array_values(array_filter([
+        env('APP_FRONTEND_URL'),
         'http://localhost:5173',
-    ],
+        'http://127.0.0.1:5173',
+    ])),
 
     'allowed_origins_patterns' => [],
 
